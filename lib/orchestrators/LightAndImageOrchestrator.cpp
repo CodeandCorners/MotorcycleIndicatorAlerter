d6f8764cb3285869ba77a152c2c5b99fe0c71893
code and corners <18582901+chill300@users.noreply.github.com>
2026-07-21 17:42:44 +0100

    tidy

--- lib/orchestrators/LightAndImageOrchestrator.cpp
+++ lib/orchestrators/LightAndImageOrchestrator.cpp
@@ -2,23 +2,36 @@
 
 void LightAndImageOrchestrator::run(){
     DirectionEnum direction = lds.calculateDirection();
+
     max7219.turnOffAll();
+
     if(direction == DirectionEnum::LEFT_ON) {
         max7219.setLeftImage();
+        
     } else if (direction == DirectionEnum::LEFT_ON_TOO_LONG) {
-        //TODO: Exclamation and then left image, flashing, will need delay
+        //Alert user
         max7219.setLeftImage();
+        delay(DELAY_PER_RUN);
+        max7219.turnOffAll();
+        max7219.setOnTooLong();
+
     } else if (direction == DirectionEnum::RIGHT_ON) {
         max7219.setRightImage();
+
     } else if (direction == DirectionEnum::RIGHT_ON_TOO_LONG) {
-        //TODO: Exclamation and then right image, flashing, will need delay
+        //Alert user
         max7219.setRightImage();
+        delay(DELAY_PER_RUN);
+        max7219.turnOffAll();
+        max7219.setOnTooLong();
+
     } else if (direction == DirectionEnum::BOTH_ON || direction == DirectionEnum::BOTH_ON_TOO_LONG) {
-        //TODO generic 2 arrows, hazards scenario
+        max7219.setBothImage();
+
     } else {
         // DO nothing
         Serial.println("No output");
     }
-    // supports flashing, supports processing compute costs
-      delay(DELAY_PER_RUN);
+     delay(DELAY_PER_RUN);
+
 };
