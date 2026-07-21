78d89bc112bf16dc4fb340caaa57f5496aa99b24
code and corners <18582901+chill300@users.noreply.github.com>
2026-07-21 21:22:11 +0100

    done

--- lib/hardware/LM393.h
+++ lib/hardware/LM393.h
@@ -7,12 +7,14 @@ class LM393 {
         explicit LM393(uint8_t pin): pin(pin){};
 
         void connect();
-        
+
         LM393Enum reading();
     private:
         uint8_t pin;
         int read();
+
         bool isFlashing = false;
+        
         const unsigned long FLASH_TIMEOUT = 15000; // 15s limit
         const unsigned long BLINK_GAP_TIME = 1500;  // 1.5s max pause between blinks
 
